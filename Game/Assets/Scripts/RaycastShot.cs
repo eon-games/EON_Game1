234700e9e7f55e95c35ac144f337c07bb6ea858f
pablogalve <eduinvestor100@gmail.com>
2019-07-03 04:18:49 +0200

    s

---
+++ Game/Assets/Scripts/RaycastShot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShot : MonoBehaviour
+{
+    public float damage = 10;
+    public float range = 100;
+    public LayerMask layer;
+    RaycastHit hit;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E)) {
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, range, layer)){
+            }
+            Debug.DrawRay(transform.position, transform.right *range, Color.green);
+        }
+    }
+}
