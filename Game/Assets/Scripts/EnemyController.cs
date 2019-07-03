7fb9859b7ab3043c142663985e54e86c2d0e8a2e
pablogalve <eduinvestor100@gmail.com>
2019-07-03 04:56:30 +0200

    Enemy AI patrol working

---
+++ Game/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if(waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    private void Move()
+    {
+        if(waypointIndex <= waypoints.Length - 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+            waypoints[waypointIndex].transform.position,
+            moveSpeed*Time.deltaTime);
+
+            if(transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
